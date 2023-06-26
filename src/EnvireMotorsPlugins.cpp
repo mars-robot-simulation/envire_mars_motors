14f7ccf2adece790ad8aca9cc1d3e65837cb944d
Anna Born <anna.born@dfki.de>
2023-06-26 10:47:14 +0200

    adapt the addMotor interface

--- src/EnvireMotorsPlugins.cpp
+++ src/EnvireMotorsPlugins.cpp
@@ -139,7 +139,7 @@ namespace mars
 
             // todo: use shared_ptr in motor
             // TODO: add MotorInterface and store it in the graph instead of SimMotor
-            unsigned long motorId = ControlCenter::motors->addMotor(&motorData, joint.get());
+            unsigned long motorId = ControlCenter::motors->addMotor(&motorData, joint.get(), frameId);
             // TODO: we should replace SimMotor by MotorInterface how it was done for joints
             //std::shared_ptr<SimMotor> motor;
             //motor.reset(control->motors->getSimMotor(motorId));
