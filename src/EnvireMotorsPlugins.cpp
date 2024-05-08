4cb4f894cbd2923b1df5badec53180f2175fdf33
Julian Liersch <julian.liersch@dfki.de>
2024-05-08 14:11:47 +0200

    Cleanup

--- src/EnvireMotorsPlugins.cpp
+++ src/EnvireMotorsPlugins.cpp
@@ -73,8 +73,8 @@ namespace mars
             bool done = false;
             while(!done)
             {
-                const envire::core::GraphTraits::vertex_descriptor vertex = envireGraph->vertex(frame);
-                envire::core::GraphTraits::vertex_descriptor parentVertex = graphTreeView->tree[vertex].parent;
+                const auto& vertex = envireGraph->vertex(frame);
+                const auto& parentVertex = graphTreeView->tree[vertex].parent;
                 // todo: check if this check is correct
                 if(parentVertex)
                 {
@@ -82,7 +82,7 @@ namespace mars
                     try
                     {
                         using SubControlItem = envire::core::Item<std::shared_ptr<SubControlCenter>>;
-                        envire::core::EnvireGraph::ItemIterator<SubControlItem> it = envireGraph->getItem<SubControlItem>(frame);
+                        const auto& it = envireGraph->getItem<SubControlItem>(frame);
                         return it->getData();
                     }
                     catch (...)
@@ -97,30 +97,33 @@ namespace mars
             return nullptr;
         }
 
+        // TODO use template method
         void EnvireMotorsPlugins::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::motors::DC>>& e)
         {
-            envire::base_types::motors::DC& motor = e.item->getData();
-            ConfigMap config = motor.getFullConfigMap();
+            auto& motor = e.item->getData();
+            auto config = motor.getFullConfigMap();
             createMotor(config, e.frame);
         }
 
+        // TODO use template method
         void EnvireMotorsPlugins::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::motors::PID>>& e)
         {
-            envire::base_types::motors::PID& motor = e.item->getData();
-            ConfigMap config = motor.getFullConfigMap();
+            auto& motor = e.item->getData();
+            auto config = motor.getFullConfigMap();
             createMotor(config, e.frame);
         }
 
+        // TODO use template method
         void EnvireMotorsPlugins::itemAdded(const envire::core::TypedItemAddedEvent<envire::core::Item<::envire::base_types::motors::DirectEffort>>& e)
         {
-            envire::base_types::motors::DirectEffort& motor = e.item->getData();
-            ConfigMap config = motor.getFullConfigMap();
+            auto& motor = e.item->getData();
+            auto config = motor.getFullConfigMap();
             createMotor(config, e.frame);
         }
 
         void EnvireMotorsPlugins::itemRemoved(const envire::core::TypedItemRemovedEvent<envire::core::Item<std::shared_ptr<core::SimMotor>>>& e)
         {
-            const unsigned long motorID = ControlCenter::motors->getID(e.item->getData()->getName());
+            const auto& motorID = ControlCenter::motors->getID(e.item->getData()->getName());
             ControlCenter::motors->removeMotor(motorID);
         }
 
@@ -128,7 +131,7 @@ namespace mars
         {
             using JointInterfaceItemItr = envire::core::EnvireGraph::ItemIterator<envire::core::Item<JointInterfaceItem>>;
 
-            std::shared_ptr<interfaces::SubControlCenter> subControl = getControlCenter(frameId);
+            auto subControl = getControlCenter(frameId);
             if (!subControl)
             {
                 return;
@@ -141,25 +144,25 @@ namespace mars
             motorData.fromConfigMap(&config, "");
 
             // there should be one joint and one motor in the same frame
-            const size_t num_joints = envireGraph->getItemCount<envire::core::Item<JointInterfaceItem>>(frameId);
+            const auto num_joints = envireGraph->getItemCount<envire::core::Item<JointInterfaceItem>>(frameId);
             if (num_joints == 0)
             {
-                const std::string errmsg{"Can not create a motor, since the frame " + frameId + " does not contain any joint interface item."};
+                const auto errmsg = std::string{"Can not create a motor, since the frame " + frameId + " does not contain any joint interface item."};
                 LOG_ERROR("%s", errmsg.c_str());
                 return;
             } 
             else if (num_joints > 1)
             {
-                const std::string errmsg{"Can not create a motor, since there are multiple joint interface items in the frame " + frameId + "."};
+                const auto errmsg = std::string{"Can not create a motor, since there are multiple joint interface items in the frame " + frameId + "."};
                 LOG_ERROR("%s", errmsg.c_str());
                 return;
             }
 
-            JointInterfaceItemItr jointItemItr = envireGraph->getItem<envire::core::Item<JointInterfaceItem>>(frameId);
-            const std::shared_ptr<JointInterface> joint = jointItemItr->getData().jointInterface;
+            auto jointItemItr = envireGraph->getItem<envire::core::Item<JointInterfaceItem>>(frameId);
+            const auto joint = jointItemItr->getData().jointInterface;
             if (!joint)
             {
-                const std::string errmsg{"Can not create motor, there is a joint interface item in the frame " + frameId + ", but joint interface is not set."};
+                const auto errmsg = std::string{"Can not create motor, there is a joint interface item in the frame " + frameId + ", but joint interface is not set."};
                 LOG_ERROR("%s", errmsg.c_str());
                 return;
             }
@@ -168,16 +171,16 @@ namespace mars
             joint->getName(&jointName);
             if (jointName != motorData.jointName)
             {
-                const std::string errmsg{"Can not create a motor, since the found joint interface does not correposponded to the motor by its name. Joint name required by motor: " + motorData.jointName + ". Found joint name: " + jointName};
+                const auto errmsg = std::string{"Can not create a motor, since the found joint interface does not correposponded to the motor by its name. Joint name required by motor: " + motorData.jointName + ". Found joint name: " + jointName};
                 LOG_ERROR("%s", errmsg.c_str());
                 return;
             }
             motorData.jointIndex = ControlCenter::jointIDManager->getID(jointName);
 
             // TODO: add MotorInterface and store it in the graph instead of SimMotor
-            const unsigned long motorId = motors->addMotor(&motorData);
+            const auto& motorId = motors->addMotor(&motorData);
             // TODO: we should replace SimMotor by MotorInterface how it was done for joints
-            std::shared_ptr<mars::core::SimMotor> motor{motors->getSimMotor(motorId)};
+            const std::shared_ptr<core::SimMotor> motor{motors->getSimMotor(motorId)};
             envire::core::Item<std::shared_ptr<mars::core::SimMotor>>::Ptr motorItemPtr{new envire::core::Item<std::shared_ptr<mars::core::SimMotor>>(motor)};
             envireGraph->addItemToFrame(frameId, motorItemPtr);
         }
